bed83450198781f7636fd3b99c68567c9105754d
sudomayur <mayur.khanna@slu.edu>
2021-07-20 22:24:28 -0500

    Added problem sets

--- Rosalind/CGContent.py
+++ Rosalind/CGContent.py
@@ -1,11 +1,11 @@
 from Bio import SeqIO
 from Bio.SeqUtils import GC
+
 name = ''
 max = 0
 for seq_record in SeqIO.parse("rosalind_gc.txt", "fasta"):
-    x = GC(seq_record.seq)
-    if x > max:
-        max = x
+    if GC(seq_record.seq) > max:
+        max = GC(seq_record.seq)
         name = seq_record.id
 
 print(name)
