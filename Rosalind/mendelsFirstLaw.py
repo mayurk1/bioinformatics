5127b0095f82cc7b0d6cc2d45de1b89aff5553cc
sudomayur <mayur.khanna@slu.edu>
2021-07-24 11:50:27 -0500

    Added problems.

---
+++ Rosalind/mendelsFirstLaw.py
@@ -0,0 +1,16 @@
+def probability(k, m, n):
+    sum = k + m + n
+    # Probability of having at least one
+    # dominant allele is = 
+    # 1 - P(no dominant alleles)
+
+    rr = (n/sum) * ((n-1)/(sum - 1))
+    hh = (m/sum) * ((m-1)/(sum - 1))
+    hr = (n/sum) * ((m)/(sum - 1)) + (m/sum) * ((n)/(sum - 1))
+
+    q = rr + 0.25*hh + 0.5*hr
+
+    return 1 - q
+
+
+print(probability(15, 19, 29))
